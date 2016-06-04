5c4ddde09c243de33a9d19278bb9b138fd76da15
Ian Wu <onlinemad@gmail.com>
2016-06-05 00:30:08 +0800

    Modified source code for generate fbia.xml

--- index.js
+++ index.js
@@ -4,25 +4,13 @@
 var assign = require('object-assign');
 var pathFn = require('path');
 
-var config = hexo.config.feed = assign({
-  type: 'atom',
-  limit: 20,
-  hub: '',
-  content: true
-}, hexo.config.feed);
-
-var type = config.type.toLowerCase();
-
-// Check feed type
-if (type !== 'atom' && type !== 'rss2') {
-  config.type = 'atom';
-} else {
-  config.type = type;
-}
+var config = hexo.config.fbia = assign({
+  limit: 20
+}, hexo.config.fbia);
 
 // Set default feed path
 if (!config.path) {
-  config.path = config.type + '.xml';
+  config.path = 'fbia.xml';
 }
 
 // Add extension name if don't have
@@ -30,4 +18,4 @@ if (!pathFn.extname(config.path)) {
   config.path += '.xml';
 }
 
-hexo.extend.generator.register('feed', require('./lib/generator'));
+hexo.extend.generator.register('fbia', require('./lib/generator'));
